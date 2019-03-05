6f596fbfcab86a563cda377a36d6014767ff770f
Davide <dg3g18@soton.ac.uk>
2019-03-05 11:45:42 +0000

    Countreviews.sh now shows a sorted list of hotels by the numbers of reviews. The output of the script shows the name of the hotel with the number of reviews next to it.

--- countreviews.sh
+++ countreviews.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-fgrep -c "<Author>" $1
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+dir= cd "$parent_path/$1"
+for file in "$dir"*.dat 
+do
+	basename -z $file .dat
+	echo -n " " 
+       	fgrep -c "<Author>" $file
+done|sort -k 2 -nr
