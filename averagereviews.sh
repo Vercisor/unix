c4386f653ff4ed0627f2308704eccc5ac57e09ec
Davide <dg3g18@soton.ac.uk>
2019-03-11 17:32:45 +0000

    averagereviews was not changed significantly, countreviews was changed so that "basename" was not used as it could lead to errors in the eventuality that basename is not installed on the machine

--- averagereviews.sh
+++ averagereviews.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+parent_path=$( cd "$(dirname "${BASH_SOURCE[-1]}")" ; pwd -P )
 dir= cd "$parent_path/$1"
-
-for file in "$dir"*.dat 
+for file in "$dir"*.dat
 do
 	hotelName=${file%.*}
 	hotelName=${hotelName##*/}
 	average=$(awk -F '>' '/<Overall>/ {count+= $2; total += 1} END{print count/total}' $file)
-	echo $hotelName $average	
-done|sort -k 2 -nr
+	echo $hotelName $average
+done|sort -k2 -nr
--- countreviews.sh
+++ countreviews.sh
@@ -3,7 +3,7 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 dir= cd "$parent_path/$1"
 for file in "$dir"*.dat 
 do
-	basename -z $file .dat
-	echo -n " " 
-       	fgrep -c "<Author>" $file
-done|sort -k 2 -nr
+	fileName=${file%.*}
+	fileName=${fileName##*/} 
+	echo $fileName $(grep -c "<Author>" $file) 
+done | sort -k 2 -nr
