634d728dd941830c835ac5f23c1e1baa1b4bb60e
Davide <dg3g18@soton.ac.uk>
2019-03-11 16:04:54 +0000

    Updated averagereviews, so that it prints the name of the hotel (it was printing a sample string "hello" before, instead of the name)

--- averagereviews.sh
+++ averagereviews.sh
@@ -4,8 +4,8 @@ dir= cd "$parent_path/$1"
 
 for file in "$dir"*.dat 
 do
-	hotelName="hello"
-	#hotelName=$(basename -z $file .dat)
+	hotelName=${file%.*}
+	hotelName=${hotelName##*/}
 	average=$(awk -F '>' '/<Overall>/ {count+= $2; total += 1} END{print count/total}' $file)
 	echo $hotelName $average	
 done|sort -k 2 -nr
