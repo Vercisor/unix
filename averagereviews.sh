330651b4d396c0aced7efd06c79b93fc46df98e5
Davide <dg3g18@soton.ac.uk>
2019-03-12 14:30:19 +0000

    The script now rounds up numbers to the second decimal place

--- averagereviews.sh
+++ averagereviews.sh
@@ -6,5 +6,6 @@ do
 	hotelName=${file%.*}
 	hotelName=${hotelName##*/}
 	average=$(awk -F '>' '/<Overall>/ {count+= $2; total += 1} END{print count/total}' $file)
-	echo $hotelName $average
+	echo -n $hotelName 
+       	printf " %0.2f\n" $average
 done|sort -k2 -nr
